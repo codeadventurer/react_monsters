e67dc658786d8603d1d04718cc80b44642fc855a
Anna Dontcova <a.dontcova@gmail.com>
2019-08-22 16:40:47 +0200

    Updates

---
+++ precache-manifest.96d206fb1182e5c703347086142ef76e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31722928ac137e3aed21448bb8164ec2",
+    "url": "/react_monsters/index.html"
+  },
+  {
+    "revision": "8451b1df938d65f5b466",
+    "url": "/react_monsters/static/css/main.a83d92cf.chunk.css"
+  },
+  {
+    "revision": "2ab373573a2da60aeefa",
+    "url": "/react_monsters/static/js/2.b3f2c5c4.chunk.js"
+  },
+  {
+    "revision": "8451b1df938d65f5b466",
+    "url": "/react_monsters/static/js/main.814e08c8.chunk.js"
+  },
+  {
+    "revision": "e0083aa1ab978db3746f",
+    "url": "/react_monsters/static/js/runtime~main.5840a39f.js"
+  }
+]);
